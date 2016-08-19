f8669f26d7aaf1e4bc034d611be4d1b8ab8fa365
Ostap Matskiv <ostap.matskiv@gmail.com>
2016-08-19 14:29:58 +0300

    Верстка за замовчуванням. Хедер, головна, футер.

--- app/views/page_view.php
+++ app/views/page_view.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
 <html lang="uk">
-
 <head>
-    <title><?=$_SESSION['alias']->title?></title>
+	<title><?=$_SESSION['alias']->title?></title>
 
     <!-- Meta -->
     <meta charset="utf-8">
@@ -11,72 +9,41 @@
     <meta name="keywords" content="<?=$_SESSION['alias']->keywords?>">
     <meta name="author" content="webspirit.com.ua">
 
-<!doctype html>
-<html class="no-js" lang="en">
-<head>
-<meta charset="utf-8">
+	<link rel="shortcut icon" href="<?=SERVER_URL?>style/admin/images/whitelion-black.png">
 
-	<link rel="shortcut icon" href="<?=IMG_PATH?>ico.jpg">
-
-	<link href="<?=SITE_URL?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-	<link href="<?=SITE_URL?>assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-	<link href="<?=SITE_URL?>assets/css/animate.min.css" rel="stylesheet" />
-	<link href="<?=SITE_URL?>assets/css/style.min.css" rel="stylesheet" />
-	<link href="<?=SITE_URL?>assets/css/style-responsive.min.css" rel="stylesheet" />
-	<link href="<?=SITE_URL?>assets/css/theme/default.css" id="theme" rel="stylesheet" />
+	<link href="<?=SERVER_URL?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?=SERVER_URL?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="<?=SERVER_URL?>style/animate.min.css" rel="stylesheet" />
+	<link href="<?=SERVER_URL?>style/style.min.css" rel="stylesheet" />
+	<link href="<?=SERVER_URL?>style/style-responsive.min.css" rel="stylesheet" />
 	<!-- ================== END BASE CSS STYLE ================== -->
     
 	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="<?=SITE_URL?>assets/plugins/pace/pace.min.js"></script>
+	<script src="<?=SERVER_URL?>assets/pace/pace.min.js"></script>
 	<!-- ================== END BASE JS ================== -->
 </head>
 <body>
-	<script>
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '1280962155267129',
-	      xfbml      : true,
-	      version    : 'v2.6'
-	    });
-	  };
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
-	
 	<?php
-		// include "@commons/mobile_menu.php";
-
 		echo('<div id="wrapper">');
 
 		include "@commons/header.php";
 
 		if(isset($view_file)) require_once($view_file.'.php');
 
-
 		include "@commons/footer.php";
 
 		echo('</div>');
 	?>
-	<script src="<?=SITE_URL?>assets/plugins/jquery/jquery-1.9.1.min.js"></script>
-	<script src="<?=SITE_URL?>assets/plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
-	<script type="text/javascript" src="<?=SITE_URL?>assets/bootstrap/js/bootstrap.min.js"></script>
-	
-
-	<script src="<?=SITE_URL?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?=SERVER_URL?>assets/jquery/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="<?=SERVER_URL?>assets/jquery/jquery-migrate-1.1.0.min.js"></script>
+	<script type="text/javascript" src="<?=SERVER_URL?>assets/bootstrap/js/bootstrap.min.js"></script>
 	<!--[if lt IE 9]>
-		<script src="assets/crossbrowserjs/html5shiv.js"></script>
-		<script src="assets/crossbrowserjs/respond.min.js"></script>
-		<script src="assets/crossbrowserjs/excanvas.min.js"></script>
+		<script src="<?=SERVER_URL?>assets/crossbrowserjs/html5shiv.js"></script>
+		<script src="<?=SERVER_URL?>assets/crossbrowserjs/respond.min.js"></script>
+		<script src="<?=SERVER_URL?>assets/crossbrowserjs/excanvas.min.js"></script>
 	<![endif]-->
-	<script src="<?=SITE_URL?>assets/plugins/jquery-cookie/jquery.cookie.js"></script>
-	<script src="<?=SITE_URL?>assets/js/apps.min.js"></script>
+	<script src="<?=SERVER_URL?>assets/jquery-cookie/jquery.cookie.js"></script>
+	<script src="<?=SERVER_URL?>assets/color-admin/apps.min.js"></script>
 	<!-- ================== END BASE JS ================== -->
 	
 	<script>
@@ -84,8 +51,5 @@
 	        App.init();
 	    });
 	</script>
-
-
-
 </body>
 </html>

10f33d5376002a92b4108ed5c2de04b4b64621db
Ostap Matskiv <ostap.matskiv@gmail.com>
2016-09-20 14:07:17 +0300

    faq+

--- app/services/faq/views/index_view.php
+++ app/services/faq/views/index_view.php
@@ -1,19 +1,8 @@
-<!--=== Breadcrumbs ===-->
-<div class="breadcrumbs">
-    <div class="container">
-        <h1 class="pull-left"><?=$_SESSION['alias']->name?></h1>
-        <ul class="pull-right breadcrumb">
-            <li><a href="<?=SITE_URL?>"><?=$this->text('Головна сторінка')?></a></li>
-            <li class="active"><?=$_SESSION['alias']->name?></li>
-        </ul>
-    </div>
-</div><!--/breadcrumbs-->
-<!--=== End Breadcrumbs ===-->
-
 <!--=== Content Part ===-->
 <div class="container content">		
-	<div class="row">            
+	<div class="row">     
         <div class="col-md-9">
+        <h1 class="list-title gray"><strong><?=$_SESSION['alias']->name?></strong></h1>
             <?php 
             if($groups) 
             {
@@ -72,29 +61,6 @@
                 }
             }
         ?>
-		</div><!--/col-md-9-->
-        
-            <div class="col-md-3">
-                <!-- Contacts -->
-                <div class="headline"><h2><?=$this->text('Контакти')?></h2></div>
-                <ul class="list-unstyled who margin-bottom-30">
-                    <li><i class="fa fa-home"></i><?=$this->text('вул. Любінська 92, Будинок меблів,')?><br><?=$this->text('м. Львів, Україна')?></li>
-                    <li><i class="fa fa-envelope"></i><?=$this->text('Email: info@webspirit.com.ua')?></li>
-                    <li><i class="fa fa-phone"></i><?=$this->text('тел: +38 067 3141471')?> </li>
-                    <li><i class="fa fa-phone"></i><?=$this->text('тел: +38 093 1289017')?> </li>
-                </ul>
-
-                <!-- Business Hours -->
-                <div class="headline"><h2><?=$this->text('Ми працюємо')?></h2></div>
-                <ul class="list-unstyled margin-bottom-30">
-                    <li><strong><?=$this->text('Понеділок-П\'ятниця:')?></strong><?=$this->text('з 10:00 до 19:00')?> </li>
-                    <li><strong><?=$this->text('Субота: ')?></strong><?=$this->text(' з 11:00 до 16:00')?></li>
-                    <li><strong><?=$this->text('Неділя: ')?></strong><?=$this->text(' Ми відпочиваємо :)')?></li>
-                </ul>
-
-                <!-- Why we are? -->
-                <div class="headline"><h2><?=$this->text('Чому - ми?')?></h2></div>
-                <p><?=$this->text('Задоволений замовник - це безцінно, отже ми працюємо на совість. Зрештою, нам просто подобається працювати якісно, швидко, відповідально і водночас творчо над кожним проектом.')?></p> 
-            </div><!--/col-md-3-->            		
+		</div><!--/col-md-9-->          		
     </div><!--/row-->
 </div><!--/container-->

5bef60f59204959ad0b59c299683c4f461a81587
Ostap Matskiv <ostap.matskiv@gmail.com>
2016-09-26 21:55:40 +0300

    Розпочато роботу над статистикою сайту. Не працює фільтр, видача інформації працює

--- app/models/wl_analytic_model.php
+++ app/models/wl_analytic_model.php
@@ -6,16 +6,52 @@ class wl_analytic_model {
 	{
 		$day = strtotime('today') - 30*3600*24;
 		$views = $this->db->getQuery("SELECT SUM(`unique`) as totalUsers, SUM(`cookie`) as newUsers, SUM(`views`) as viewsCount FROM `wl_statistic_views` WHERE `day` >= {$day}");
-		
-		if($views->totalUsers == 0) $views->totalUsers = 1;
+		if($views)
+		{
+			if($views->totalUsers == 0) $views->totalUsers = 1;
 
-		$views->returnedUser = $views->totalUsers - $views->newUsers;
-		$views->returnedPercentage = round($views->returnedUser * 100 / $views->totalUsers, 1) ;
-		$views->newPercentage = round($views->newUsers * 100 / $views->totalUsers, 1)  ;
+			$views->returnedUser = $views->totalUsers - $views->newUsers;
+			$views->returnedPercentage = round($views->returnedUser * 100 / $views->totalUsers, 1) ;
+			$views->newPercentage = round($views->newUsers * 100 / $views->totalUsers, 1)  ;
 
-		$views->tableData = $this->db->getQuery("SELECT * FROM `wl_statistic_views` WHERE `day` >= {$day}", 'array');
-
+			$views->tableData = $this->db->getQuery("SELECT * FROM `wl_statistic_views` WHERE `day` >= {$day}", 'array');
+		}
 		return $views; 
+	}
+
+	public function getStatistic()
+	{
+		$where = array();
+		if($alias = $this->data->get('alias'))
+		{
+			$where['alias'] = $alias;
+			if($content = $this->data->get('content')) $where['content'] = $content;
+		}
+		if($language = $this->data->post('language')) $where['language'] = $language;
+		if($min = $this->data->get('min'))
+		{
+			$where['day'] = '>='.$min;
+		}
+		if($max = $this->data->get('max'))
+		{
+			$where['day'] = '<='.$max;
+		}
+		if(empty($where))
+			$this->db->select('wl_statistic_pages as s');
+		else
+			$this->db->select('wl_statistic_pages as s', '*', $where);
+		$this->db->join('wl_sitemap', 'link', array('alias' => '#s.alias', 'content' => '#s.content'));
+		$this->db->order('id DESC');
+		if(isset($_SESSION['option']->paginator_per_page) && $_SESSION['option']->paginator_per_page > 0)
+		{
+			$start = 0;
+			if(isset($_GET['per_page']) && is_numeric($_GET['per_page']) && $_GET['per_page'] > 0)
+				$_SESSION['option']->paginator_per_page = $_GET['per_page'];
+			if(isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 1)
+				$start = ($_GET['page'] - 1) * $_SESSION['option']->paginator_per_page;
+			$this->db->limit($start, $_SESSION['option']->paginator_per_page);
+		}
+		return $this->db->get('array');
 	}
 
 } 
---
+++ app/views/admin/wl_statistic/index_view.php
@@ -0,0 +1,85 @@
+<div class="row">
+	<div class="col-md-4 ui-sortable">
+        <div class="panel panel-inverse" data-sortable-id="form-plugins-1">
+	        <div class="panel-heading">
+	            <h4 class="panel-title">Фільтр</h4>
+	        </div>
+	        <div class="panel-body panel-form">
+	            <form class="form-horizontal form-bordered" _lpchecked="1">
+	                <div class="form-group">
+	                    <label class="col-md-3 control-label">Оберіть період</label>
+	                    <div class="col-md-9">
+	                        <div class="input-group input-daterange">
+	                            <input type="text" class="form-control" name="start" placeholder="Date Start">
+	                            <span class="input-group-addon">-</span>
+	                            <input type="text" class="form-control" name="end" placeholder="Date End">
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="form-group">
+	                	<label class="col-md-3 control-label">Статистика по </label>
+                        <div class="col-md-9">
+		                	<select name="alias" class="form-control">
+		                		<option value="">Всіх адресах</option>
+		                		<?php $aliases = $this->db->getAllData('wl_aliases');
+		                		foreach ($aliases as $alias) {
+		                			echo("<option value='{$alias->id}'>{$alias->alias}</option>");
+		                		}
+		                		?>
+		                	</select>
+						</div>
+	                </div>
+	                <div class="form-group">
+                        <label class="col-md-3 control-label"></label>
+                        <div class="col-md-9">
+                            <button type="submit" class="btn btn-sm btn-success">Пошук</button>
+                        </div>
+                    </div>
+	            </form>
+	        </div>
+	    </div>
+    </div>
+    <div class="col-md-8 ui-sortable">
+    	<?php require_once APP_PATH.'views'.DIRSEP.'admin'.DIRSEP.'@commons'.DIRSEP.'_wl_statistic.php'; ?>
+    </div>
+</div>
+<div class="row">
+	<div class="col-md-12 ui-sortable">
+		<div class="panel panel-inverse" data-sortable-id="form-plugins-2">
+			<div class="panel-body">
+				<table class="table table-striped table-responsive table-hover">
+		            <thead>
+		                <tr>
+		                    <th>#</th>
+		                    <th>Адреса</th>
+		                    <?php if($_SESSION['language']) { ?>
+		                    	<th>Мова</th>
+		                    <?php } ?>
+		                    <th>День</th>
+		                    <th>Унікальні відвідувачі</th>
+		                    <th>Загальні перегляди</th>
+		                </tr>
+		            </thead>
+		            <tbody>
+		            	<?php if($wl_statistic) foreach ($wl_statistic as $statistic) { ?>
+			                <tr>
+			                    <td><?=$statistic->id?></td>
+			                    <td><a href="<?=SITE_URL?><?=($statistic->link == 'main')?'':$statistic->link?>" target="_blank">/<?=$statistic->link?></a></td>
+			                    <?php if($_SESSION['language']) { ?>
+			                    	<td><?=$statistic->language?></td>
+			                    <?php } ?>
+			                    <td><?=date('d.m.Y', $statistic->day)?></td>
+			                    <td><?=$statistic->unique?></td>
+			                    <td><?=$statistic->views?></td>
+			                </tr>
+		                <?php } ?>
+		            </tbody>
+		        </table>
+		        <?php
+	                $this->load->library('paginator');
+	                echo $this->paginator->get();
+	            ?>
+	        </div>
+	    </div>
+    </div>
+</div>

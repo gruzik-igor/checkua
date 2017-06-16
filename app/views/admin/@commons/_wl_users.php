c3a9e8d04ed70c791fb3be287c0836dd30dc6309
Roman Konyk <ygrenne@gmail.com>
2017-06-16 17:16:48 +0300

    fixed admin main view user ui bug

--- app/views/admin/@commons/_wl_users.php
+++ app/views/admin/@commons/_wl_users.php
@@ -1,24 +1,25 @@
 <!-- begin panel -->
 <div class="panel panel-inverse" data-sortable-id="index-4">
     <div class="panel-heading">
-        <h4 class="panel-title">Новозареєстровані користувачі 
+        <h4 class="panel-title">Новозареєстровані користувачі
             <span class="pull-right label label-success">Всіх підтверджених користувачів: <?=$this->db->getCount('wl_users', 1, 'status')?></span>
         </h4>
     </div>
     <ul class="registered-users-list clearfix">
         <?php if($users = $this->db->getAllDataByFieldInArray('wl_users', array('status' => '!3'), 'id DESC LIMIT 8'))
+            $count = 0;
             foreach ($users as $user) {
                 $link = 'javascript:;';
                 if($_SESSION['user']->admin == 1) $link = SITE_URL.'admin/wl_users/'.$user->email;
         ?>
-            <li>
-                <a href="<?=$link?>"><img src="<?=SERVER_URL?>style/admin/images/user-<?=$user->type?>.jpg" alt="<?=$user->name?>"></a>
+            <li <?= $count == 4 ? 'style="clear:both"' : ''?>>
+                <a href="<?=$link?>"><img src="<?= ($user->photo > 0)? IMG_PATH.'profile/'.$user->id.'.jpg' : SERVER_URL.'style/admin/images/user-'.$user->type.'.jpg'  ?>" alt="<?=$user->name?>"></a>
                 <h4 class="username text-ellipsis">
                     <?=$user->name?>
                     <small><?=date('d.m.Y H:i', $user->registered)?></small>
                 </h4>
             </li>
-        <?php } ?>
+        <?php $count++; } ?>
     </ul>
     <?php if($_SESSION['user']->admin == 1) { ?>
         <div class="panel-footer text-center">

13975c90bc8f3093c3e46869c8263295d236b974
Ostap Matskiv <ostap.matskiv@gmail.com>
2017-09-19 13:56:15 +0300

    fix admin forms name fields

--- app/views/admin/wl_forms/info_view.php
+++ app/views/admin/wl_forms/info_view.php
@@ -2,8 +2,11 @@
     <div class="col-md-12">
         <div class="panel panel-inverse">
             <div class="panel-heading">
-                <div class="panel-heading-btn">
-                </div>
+                <?php if($_SESSION['user']->admin) { ?>
+                    <div class="panel-heading-btn">
+                        <a href="<?=SITE_URL?>admin/wl_forms/<?=$this->data->uri(3)?>" class="btn btn-warning btn-xs"><i class="fa fa-cogs"></i> До налаштування форми</a>
+                    </div>
+                <?php } ?>
                 <h4 class="panel-title">Інформація:</h4>
             </div>
             <div class="panel-body">
@@ -12,23 +15,33 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <?php if($formInfo) foreach ($formInfo as $info) { ?>
-                                <th><?= $info->title?></th>
-                                <?php } ?>
+                                <?php if($formInfo)
+                                {
+                                    if($tableInfo)
+                                    {
+                                        foreach ($tableInfo[0] as $key => $value) {
+                                            foreach ($formInfo as $info) {
+                                                if($info->name == $key)
+                                                    echo '<th>'.$info->title.'</th>';
+                                            }
+                                        }
+                                    }
+                                    else
+                                        foreach ($formInfo as $info) {
+                                            echo '<th>'.$info->title.'</th>';
+                                        }
+                                } ?>
                                 <th>Дата додачі</th>
+                                <th>Мова сайту</th>
                             </tr>
                         </thead>
                         <tbody>
-                        	<?php if($tableInfo) foreach ($tableInfo as $info) {  ?>
-                            <tr>
-                                <?php foreach ($info as $key => $value) 
-                                { 
-                                    if($key == 'language') continue;
+                        	<?php if($tableInfo) foreach ($tableInfo as $info) {
+                                echo ('<tr>');
+                                foreach ($info as $key => $value)
                                     echo "<td>" . (preg_match('/date/', $key) ? date('d.m.Y H:i', $value) : $value) . '</td>';
-                                }
-                                ?>
-                            </tr>
-                            <?php } ?>
+                                echo ('</tr>');
+                            } ?>
                         </tbody>
                     </table>
                 </div>

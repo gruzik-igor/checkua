837bf6d8741bb5b2cc9b9e6851b825cb6becd46f
Ostap Matskiv <ostap.matskiv@gmail.com>
2018-01-04 17:11:43 +0200

    додано можливість підключати таби про користувача в панелі керування через wl_aliases_cooperation: __tab_profile

--- app/views/admin/wl_users/edit_view.php
+++ app/views/admin/wl_users/edit_view.php
@@ -16,21 +16,36 @@
 		<?php } ?>
     </div>
 </div>
-<?php unset($_SESSION['notify']->success, $_SESSION['notify']->error); } ?>
+<?php unset($_SESSION['notify']->success, $_SESSION['notify']->error); } 
+
+$additionall_tabs = array();
+if($actions = $this->db->getAllDataByFieldInArray('wl_aliases_cooperation', array('alias1' => '<0', 'type' => '__tab_profile'), 'alias1'))
+    foreach ($actions as $action) {
+        $tab = $this->load->function_in_alias($action->alias2, '__tab_profile', $user->id, true);
+        $additionall_tabs[$tab->key] = clone $tab;
+    }
+?>
 
 <div class="row">
     <div class="col-md-12">
         <ul class="nav nav-tabs" id="myTab">
             <li class="active"><a data-target="#tabs-main" href="#tabs-main" data-toggle="tab">Загальні дані</a></li>
+            <?php if(!empty($additionall_tabs))
+                    foreach ($additionall_tabs as $tab) {
+                        echo('<li><a data-target="#tabs-'.$tab->key.'" href="#tabs-'.$tab->key.'" data-toggle="tab">'.$tab->name.'</a></li>');
+                    } ?>
             <li><a data-target="#tabs-register" href="#tabs-register" data-toggle="tab">Реєстр дій</a></li>
-            <li><a data-target="#tabs-edit" href="#tabs-edit" data-toggle="tab">Редагувати</a></li>
-          
+            <li><a data-target="#tabs-edit" href="#tabs-edit" data-toggle="tab">Редагувати</a></li> 
         </ul>
 
         <div class="tab-content col-md-12">
             <div class="tab-pane active" id="tabs-main">
                 <?php require_once 'tabs/_tabs-main.php'; ?>
             </div>
+            <?php if(!empty($additionall_tabs))
+                    foreach ($additionall_tabs as $tab) {
+                        echo('<div class="tab-pane" id="tabs-'.$tab->key.'">'.$tab->content.'</div>');
+                    } ?>
             <div class="tab-pane" id="tabs-register">
                 <?php require_once 'tabs/_tabs-register.php'; ?>
             </div>
